9112df6ee1b89ec31348a51c0230260787bc799c
Conrad49 <CodingConrad49@gmail.com>
2022-01-14 23:15:48 -0500

    random z-values

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,14 +26,31 @@ public class Runner extends Application{
 		
 		double xVal = 3.14;
 		double yVal = 1.25;
-		double xStep = 0.1;
-		double yStep = 0.1;
+		double xStep = 0.01;
+		double yStep = 0.01;
 		
+		Random random = new Random();
+		double rangeMin = 1.0;
+		double rangeMax = 70.0;
+		double z1 = rangeMin + (rangeMax - rangeMin) * random.nextDouble();
+		double z2 = rangeMin + (rangeMax - rangeMin) * random.nextDouble();
+		double z3 = rangeMin + (rangeMax - rangeMin) * random.nextDouble();
 		for(int i = 0; i < 500; i ++) {
 			for(int j = 0; j < 500; j ++) {
-				double noiseVal = (generator.noise(xVal, yVal, 7) + 1.0)/2.0;
+				
+				double noiseVal = ((generator.noise(xVal, yVal, z1) + 1.0)/2.0);
+				double noiseVal2 = (generator.noise(xVal, yVal, z2) + 1.0)/2.0;
+				double noiseVal3 = (generator.noise(xVal, yVal, z3) + 1.0)/2.0;
+				
+				int r = (int)(noiseVal*255);
+				int g = (int)(noiseVal2*255);
+				int b = (int)(noiseVal3*255);
 				//System.out.println(generator.noise(xVal, yVal, 7));
-				writer.setColor(j, i, Color.rgb(0, 0, 0, noiseVal));
+				//if(noiseVal < 0.5) {					
+					writer.setColor(j, i, Color.rgb(r, g, b, 1.0));
+				//} else {
+				//	writer.setColor(j, i, Color.rgb(50, 50, 50, 1));
+				//}
 				xVal += xStep;
 			}
 			

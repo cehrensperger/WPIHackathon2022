d736535728c6f906b9b8988639eb694aa155787e
Conrad49 <CodingConrad49@gmail.com>
2022-01-14 22:02:00 -0500

    test button

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;  
+import javafx.stage.Stage;  
+import javafx.scene.layout.StackPane;  
 
-public class Runner {
+public class Runner extends Application{
 	static PerlinNoiseGenerator generator = new PerlinNoiseGenerator();
 	public static void main(String[] args) {
+		launch(args);
 		   System.out.println(generator.noise(3.14,42,7));
+	}
+	@Override
+	public void start(Stage stage) throws Exception {
+		StackPane root = new StackPane();
+		Button btn = new Button("test button");
+		root.getChildren().add(btn);
+		
+		Scene scene = new Scene(root);
+		
+		stage.setScene(scene);  
+        stage.setTitle("First JavaFX Application");  
+        stage.show();  
+		
 	}
 }
